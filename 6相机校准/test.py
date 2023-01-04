2c429e1fa32ffe598e1c8dfb3a16b44b05542928
zjhczl <2728774085@qq.com>
2023-01-04 18:07:15 +0800

    ss

---
+++ 6相机校准/test.py
@@ -0,0 +1,33 @@
+# 相机校准
+# 一些针孔相机会给图像带来明显的失真。两种主要的畸变是径向畸变和切向畸变。
+# 我们需要找到五个参数，称为失真系数
+# 除此之外，我们还需要一些其他信息，比如相机的内在和外在参数。
+import numpy as np
+import cv2 as cv
+import glob
+# 终止条件
+criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+# prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
+objp = np.zeros((6*7, 3), np.float32)
+objp[:, :2] = np.mgrid[0:7, 0:6].T.reshape(-1, 2)
+
+# print(np.mgrid[0:7, 0:6].T.reshape(-1, 2))
+# Arrays to store object points and image points from all the images.
+objpoints = []  # 3d point in real world space
+imgpoints = []  # 2d points in image plane.
+images = glob.glob('*.jpg')
+for fname in images:
+    img = cv.imread(fname)
+    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    # Find the chess board corners
+    ret, corners = cv.findChessboardCorners(gray, (7, 6), None)
+    # If found, add object points, image points (after refining them)
+    if ret == True:
+        objpoints.append(objp)
+        corners2 = cv.cornerSubPix(gray, corners, (11, 11), (-1, -1), criteria)
+        imgpoints.append(corners2)
+        # Draw and display the corners
+        cv.drawChessboardCorners(img, (7, 6), corners2, ret)
+        cv.imshow('img', img)
+        cv.waitKey(500)
+cv.destroyAllWindows()
